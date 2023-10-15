48ac747d0c0d8eb5f4ab282d47e07d75d7f14207
ARCIVAL PALMA <palma_arcival@yahoo.com>
2023-10-15 15:06:49 -0400

    setup bootsplash

--- src/storage/user.storage.ts
+++ src/storage/user.storage.ts
@@ -1,48 +1,48 @@
-import { MMKV } from 'react-native-mmkv'
-import { STORAGE_SECRET } from '@env';
+import {MMKV} from 'react-native-mmkv';
+import {STORAGE_SECRET} from '@env';
 
 // create moreStorage
 const userStorage = new MMKV({
-    id: `user-storage`,
-    path: '/',
-    encryptionKey: STORAGE_SECRET
+  id: `user-storage`,
+  path: '/',
+  encryptionKey: STORAGE_SECRET,
 });
 
 export interface userData {
-    id: number;
-    userName: string;
-    email: string;
-    token: string;
+  id: number;
+  userName: string;
+  email: string;
+  token: string;
 }
 
 export const getUserData = (): userData | undefined => {
-    const userData = userStorage.getString('user');
+  const userData = userStorage.getString('user');
 
-    if (userData) {
-        return JSON.parse(userData);
-    }
+  if (userData) {
+    return JSON.parse(userData);
+  }
 
-    return undefined;
+  return undefined;
 };
 
 export const setUserData = (data: userData): void => {
-    const stringData = JSON.stringify(data);
+  const stringData = JSON.stringify(data);
 
-    userStorage.set('user', stringData);
+  userStorage.set('user', stringData);
 };
 
 export const isDarkModeActive = (): boolean => {
-    const isDarkMode = userStorage.getBoolean('isDarkMode') ?? false;
+  const isDarkMode = userStorage.getBoolean('isDarkMode') ?? false;
 
-    return isDarkMode;
-}
+  return isDarkMode;
+};
 
 export const toggleActiveTheme = (): void => {
-    const isDarkMode = userStorage.getBoolean('isDarkMode') ?? false;
+  const isDarkMode = userStorage.getBoolean('isDarkMode') ?? false;
 
-    userStorage.set('activeTheme', !isDarkMode);
-}
+  userStorage.set('activeTheme', !isDarkMode);
+};
 
 export const clearUserStorage = (): void => {
-    userStorage.clearAll();
-}
+  userStorage.clearAll();
+};
